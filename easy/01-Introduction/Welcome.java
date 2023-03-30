ec1ae0bf3a4532f0e7693020c223fa171f51146a
meeedbkr <meeedbk00@gmail.com>
2023-03-30 20:26:55 +0000

    Welcome to java Done!

---
+++ easy/01-Introduction/Welcome.java
@@ -0,0 +1,18 @@
+// This line declares a public class named "Welcome"
+public class Welcome{
+    
+    // This line declares the main method which takes an array of strings as an argument
+    public static void main(String[] args) {
+        
+        /* This comment reminds the user to enter their own code and print output 
+         * to STDOUT. It also specifies that the name of the class should be "Welcome"
+         */
+        // Enter your code here. Print output to STDOUT. Your class should be named "Welcome"
+        
+        // This line prints "Hello, World." to the console
+        System.out.println("Hello, World.");
+        
+        // This line prints "Hello, Java." to the console
+        System.out.println("Hello, Java.");
+    }
+}
